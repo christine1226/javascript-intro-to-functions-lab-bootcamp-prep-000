08c3733e6207d33d2479e7c42f24f9ee4444f083
christine.gomez1226@gmail.com <christine.gomez1226@gmail.com>
2018-07-16 02:36:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ function logShout(string){
  console.log(lowercase);}
 
  function sayHiToGrandma(string){
- if (string ===string.toLowerCase())
+ if (string ===string.toLowerCase()){
  return "I can't hear you!";}
-
-function sayHiToGrandma(string){
-  if (string ===string.toUpperCase())
+  if (string ===string.toUpperCase()){
   return "YES INDEED!";}
